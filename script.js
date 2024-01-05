2b131717e979eefc2351dff620fab819b237e4ad
Mateus Victor <mateusvictorct@gmail.com>
2024-01-05 19:39:14 -0300

    inserindo arquivos da lampada

---
+++ script.js
@@ -0,0 +1,20 @@
+let lampadaLigada = document.getElementById('Ligada')
+let lampadaDesligar = document.getElementById('Desligar')
+let lampada = document.getElementById('lampada')
+
+function Ligada() {
+
+    lampada.src = './imagens/lampadaAcessa.jpg';
+
+}
+lampadaLigada.addEventListener('click', Ligada);
+
+function Desligar() {
+
+    lampada.src = './imagens/lampadaApagada.jpg';
+
+}
+
+lampadaDesligar.addEventListener('click', Desligar);
+lampada.addEventListener('mouseover', Ligada);
+lampada.addEventListener('mouseleave', Desligar);
